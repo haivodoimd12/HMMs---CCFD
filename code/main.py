450c0f3feff696cde3ea9d038e3286e4b6a92588
haivodoimd12 <55711042+haivodoimd12@users.noreply.github.com>
2020-07-12 16:44:21 +0700

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from clustering import KMeansClustering
+from driver import Driver
+from hidden_markov_model import HMM
+
+from config import *
+
+
+def get_input():
+    while True:
+        new_transaction = input('Nhap so tien giao dich moi: ')
+        if int(new_transaction) == TERMINATE:
+            break
+        new_transaction = k.predict(int(new_transaction))
+        new_observation = np.append(observations[1:], [new_transaction])
+
+        if h.detect_fraud(observations, new_observation, THRESHOLD):
+            print('Phat hien gian lan')
+        else:
+            print('Giao dich binh thuong')
+
+
+if __name__ == '__main__':
+    d = Driver('C:/Users/DELL/Documents/Credit-Card-Fraud-Detection-master/Data/train_data.txt')
+
+    h = HMM(n_states=STATES, n_possible_observations=CLUSTERS)
+    k = KMeansClustering()
+
+    observations = k.run(d.get_data()[0:192])
+    h.train_model(observations=list(observations), steps=STEPS)
+
+    get_input()
